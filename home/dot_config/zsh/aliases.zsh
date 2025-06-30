7a6468fd9f3798c1a8956bb62a51466785f51a35
Daniel von Eßen <daniel@vonessen.eu>
2025-06-30 15:54:39 +0200

    feat(work): :sparkles: added work shell alias and encrypted files

--- home/dot_config/zsh/aliases.zsh
+++ home/dot_config/zsh/aliases.zsh
@@ -75,4 +75,3 @@ if command -v lazygit &> /dev/null; then
     alias lg="lazygit"
 fi
 
-alias bku-sops="SOPS_AGE_KEY_FILE=$XDG_CONFIG_HOME/bku/keyfile.txt sops exec-env $XDG_CONFIG_HOME/bku/credentials.env"
---
+++ home/dot_config/zsh/work.zsh
@@ -0,0 +1,2 @@
+alias bku-sops="SOPS_AGE_KEY_FILE=$XDG_CONFIG_HOME/bku/keyfile.txt sops exec-env $XDG_CONFIG_HOME/bku/credentials.env"
+
