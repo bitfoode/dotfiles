234e5211656aa86a01f36e541d1ff8a848dfc85b
Daniel von Eßen <daniel@vonessen.eu>
2025-07-24 15:58:57 +0200

    style(nvim): :gem: removed comments

--- home/dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ home/dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -209,21 +209,8 @@ return {
           },
           filetypes = { "bash", "sh", "zsh" },
         },
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
-
         tombi = {},
         lua_ls = {
-          -- cmd = {...},
-          -- filetypes = { ...},
-          -- capabilities = {},
           settings = {
             Lua = {
               completion = {
@@ -235,8 +222,6 @@ return {
                 },
                 callSnippet = "Replace",
               },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
             },
           },
         },
