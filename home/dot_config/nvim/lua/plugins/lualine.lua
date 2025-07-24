5a8e4340f2b741dde4765c4c24251f6923175483
Daniel von Eßen <daniel@vonessen.eu>
2025-07-24 15:57:32 +0200

    feat(nvim): :sparkles: prettify statusline with lualine

--- home/dot_config/nvim/lua/plugins/lualine.lua
+++ home/dot_config/nvim/lua/plugins/lualine.lua
@@ -3,7 +3,28 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
-      options = { disabled_filetypes = { statusline = vim.g.disabled_filetypes, winbar = vim.g.disabled_filetypes } },
+      options = {
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
+        disabled_filetypes = { statusline = vim.g.disabled_filetypes, winbar = vim.g.disabled_filetypes },
+      },
+      sections = {
+        lualine_a = {
+          {
+            "mode",
+          },
+        },
+        lualine_b = {
+          {
+            "branch",
+            icon = "",
+          },
+          { "diff" },
+          {
+            "diagnostics",
+          },
+        },
+      },
       theme = "tokyonight",
     },
   },
