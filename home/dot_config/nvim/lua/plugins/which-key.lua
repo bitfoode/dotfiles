ab1012fa2e2646fd7bb5dd814db11a52cfd123c7
Daniel von Eßen <daniel@vonessen.eu>
2025-09-08 19:48:24 +0200

    feat(nvim): :sparkles: using simpler return in which-key.nvim

--- home/dot_config/nvim/lua/plugins/which-key.lua
+++ home/dot_config/nvim/lua/plugins/which-key.lua
@@ -1,37 +1,35 @@
-return {
-  -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
-  --
-  -- This is often very useful to both group configuration, as well as handle
-  -- lazy loading plugins that don't need to be loaded immediately at startup.
-  --
-  -- For example, in the following configuration, we use:
-  --  event = 'VimEnter'
-  --
-  -- which loads which-key before all the UI elements are loaded. Events can be
-  -- normal autocommands events (`:help autocmd-events`).
-  --
-  -- Then, because we use the `config` key, the configuration only runs
-  -- after the plugin has been loaded:
-  --  config = function() ... end
+return
+-- NOTE: Plugins can also be configured to run Lua code when they are loaded.
+--
+-- This is often very useful to both group configuration, as well as handle
+-- lazy loading plugins that don't need to be loaded immediately at startup.
+--
+-- For example, in the following configuration, we use:
+--  event = 'VimEnter'
+--
+-- which loads which-key before all the UI elements are loaded. Events can be
+-- normal autocommands events (`:help autocmd-events`).
+--
+-- Then, because we use the `config` key, the configuration only runs
+-- after the plugin has been loaded:
+--  config = function() ... end
+{ -- Useful plugin to show you pending keybinds.
+  "folke/which-key.nvim",
+  event = "VimEnter", -- Sets the loading event to 'VimEnter'
+  config = function() -- This is the function that runs, AFTER loading
+    require("which-key").setup()
 
-  { -- Useful plugin to show you pending keybinds.
-    "folke/which-key.nvim",
-    event = "VimEnter", -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-      require("which-key").setup()
-
-      -- Document existing key chains
-      require("which-key").add({
-        { "<leader>b", group = "[B]uffer" },
-        { "<leader>c", group = "[C]ode" },
-        { "<leader>d", group = "[D]ocument" },
-        { "<leader>f", group = "[F]ind" },
-        { "<leader>g", group = "[G]it" },
-        { "<leader>r", group = "[R]ename" },
-        { "<leader>s", group = "[S]earch" },
-        { "<leader>t", group = "[T]oggle" },
-        { "<leader>w", group = "[W]orkspace" },
-      })
-    end,
-  },
+    -- Document existing key chains
+    require("which-key").add({
+      { "<leader>b", group = "[B]uffer" },
+      { "<leader>c", group = "[C]ode" },
+      { "<leader>d", group = "[D]ocument" },
+      { "<leader>f", group = "[F]ind" },
+      { "<leader>g", group = "[G]it" },
+      { "<leader>r", group = "[R]ename" },
+      { "<leader>s", group = "[S]earch" },
+      { "<leader>t", group = "[T]oggle" },
+      { "<leader>w", group = "[W]orkspace" },
+    })
+  end,
 }
