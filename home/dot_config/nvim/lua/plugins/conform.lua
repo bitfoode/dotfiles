3968c699035785a535a73c38a80feb487b67b133
Daniel von Eßen <daniel@vonessen.eu>
2025-09-11 11:35:26 +0200

    feat(nvim): :sparkles: removed lsp_format from format_on_save, it is covered by default_format_ops

--- home/dot_config/nvim/lua/plugins/conform.lua
+++ home/dot_config/nvim/lua/plugins/conform.lua
@@ -17,7 +17,7 @@ return {
     ---@type conform.setupOpts
     opts = {
       notify_on_error = true,
-      format_on_save = { lsp_format = "fallback" },
+      format_on_save = {},
       default_format_opts = {
         lsp_format = "fallback",
       },
