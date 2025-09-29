b2974e027527211e695918df13bc3b5d49b58779
Daniel von Eßen <daniel@vonessen.eu>
2025-09-29 17:02:14 +0200

    feat: :sparkles: Added umlaut configuration to render-markdown in nvim when typing :ae or ae: to create the german umlauts

--- home/dot_config/nvim/lua/plugins/render-markdown.lua
+++ home/dot_config/nvim/lua/plugins/render-markdown.lua
@@ -22,6 +22,41 @@ return {
       },
     },
   },
+  init = function()
+    -- Put this in your init.lua or a plugin file
+
+    -- Umlaut and sharp S mappings
+    local umlaut_map = {
+      ae = "ä",
+      ue = "ü",
+      oe = "ö",
+      Ae = "Ä",
+      Ue = "Ü",
+      Oe = "Ö",
+      ss = "ß",
+    }
+
+    local function set_colon_umlaut_mappings()
+      local opts = { expr = true, noremap = true, buffer = true }
+
+      for lhs, char in pairs(umlaut_map) do
+        vim.keymap.set("i", lhs .. ":", function()
+          -- Remove colon and the letters typed, insert the umlaut/ß
+          return char
+        end, opts)
+        vim.keymap.set("i", ":" .. lhs, function()
+          -- Remove colon and the letters typed, insert the umlaut/ß
+          return char
+        end, opts)
+      end
+    end
+
+    -- Apply only to markdown files
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "markdown",
+      callback = set_colon_umlaut_mappings,
+    })
+  end,
   keys = {
     {
       "<leader>tm",
